29418a090f47833a21535e2a051a5c9af48ce626
DevmurariHarshkumar <harshkumar.devmurari@zeuslearning.com>
2024-07-18 18:09:18 +0530

    keyboard up,down,left,right added

--- task8excelfrontend/canvas.js
+++ task8excelfrontend/canvas.js
@@ -14,13 +14,15 @@ var cols = new Array(50).fill(30);
 var table = new Table(rows.length, cols.length, rows, cols);
 table.drawTable();
 
+var selected_cell = null;
+
 canvas.addEventListener("click", (event) => {
 	let rect = canvas.getBoundingClientRect();
-    // console.log("rect is :", rect);
-    let total_left = event.clientX - rect.left;
-    let total_height = event.clientY - rect.top;
-    console.log("px pos", total_height, total_left);
-	var cell = getCellFromClick(total_left, total_height)
+    let x_position = event.clientX - rect.left;
+    let y_position = event.clientY - rect.top;
+    console.log("pixel_pos", x_position, y_position);
+	var cell = getCellFromClick(x_position, y_position);
+	selected_cell = cell;
 	drawSelectedCell(cell)
 });
 
@@ -30,7 +32,7 @@ function getCellFromClick(x, y) {
 			const cell = table.table[i][j];
 			if (x >= cell.x_px && x <= cell.x_px + cell.width &&
 				y >= cell.y_px && y <= cell.y_px + cell.height) {
-				console.log(cell.x_pos, cell.y_pos)
+				console.log("cell_pos", cell.x_pos, cell.y_pos)
 				return cell;
 			}
 		}
@@ -40,6 +42,7 @@ function getCellFromClick(x, y) {
 
 function drawSelectedCell(cell) {
 	table.drawTable()
+	console.log("cell incokming")
 	c.strokeStyle = "rgba(5, 96, 242, 1)";
     c.strokeRect(cell.x_px-1, cell.y_px-1, cell.width+2, cell.height+2);
 	c.strokeRect(cell.x_px-2, cell.y_px-2, cell.width+4, cell.height+4);
@@ -65,5 +68,51 @@ function drawSelectedCell(cell) {
 	c.fillStyle = "yellow";
     c.fillRect(indexcell.x_px + 1, indexcell.y_px + 1, indexcell.width - 2, indexcell.height - 2);
 }
+
+
+canvas.addEventListener("dblclick", (event) => {
+	console.log("dblclick");
+	let rect = canvas.getBoundingClientRect();
+    let x_position = event.clientX - rect.left;
+    let y_position = event.clientY - rect.top;
+	var cell = getCellFromClick(x_position, y_position);
+	c.fillStyle = "blue";
+    c.fillRect(cell.x_px + 1, cell.y_px + 1, cell.width - 2, cell.height - 2);
+});
+
+
+window.addEventListener("keydown", (event) => {
+	console.log("keydown", event.key);
+	if (event.key == "ArrowUp"){
+		console.log("selectedcell", selected_cell);
+		var nextcell = table.table[(selected_cell.x_pos)][(selected_cell.y_pos)-1];
+		console.log("nextcell",nextcell)
+		drawSelectedCell(nextcell);
+		selected_cell = nextcell;
+	}
+	if (event.key == "ArrowDown"){
+		console.log("selectedcell", selected_cell);
+		var nextcell = table.table[(selected_cell.x_pos)][(selected_cell.y_pos)+1];
+		console.log("nextcell",nextcell)
+		drawSelectedCell(nextcell);
+		selected_cell = nextcell;
+	}
+	if (event.key == "ArrowLeft"){
+		console.log("selectedcell", selected_cell);
+		var nextcell = table.table[(selected_cell.x_pos-1)][(selected_cell.y_pos)];
+		console.log("nextcell",nextcell)
+		drawSelectedCell(nextcell);
+		selected_cell = nextcell;
+	}
+	if (event.key == "ArrowRight"){
+		console.log("selectedcell", selected_cell);
+		var nextcell = table.table[(selected_cell.x_pos+1)][(selected_cell.y_pos)];
+		console.log("nextcell",nextcell)
+		drawSelectedCell(nextcell);
+		selected_cell = nextcell;
+	}
+});
+
+
 
 export { c };
